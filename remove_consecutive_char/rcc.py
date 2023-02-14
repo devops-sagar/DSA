63e7e1439900b1f139e98405dd75b9ca01d51032
devops-sagar <pathaksagar16@gmail.com>
2023-02-14 14:33:36 -0800

    added the remove consecutive charactor probelm

---
+++ remove_consecutive_char/rcc.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param A : string
+    # @param B : integer
+    # @return a strings
+    def solve(self, A, B):
+        prev = A[0]
+        print(prev)
+
+obj =  Solution()
+obj.solve("abcd", 2)
