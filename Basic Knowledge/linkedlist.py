9da47859188b07a57e18092c7a4b6e2d05fa0e2f
devops-sagar <pathaksagar16@gmail.com>
2023-02-14 16:39:46 -0800

    added the simple linked list creation

---
+++ Basic Knowledge/linkedlist.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def set_next(self, next):
+        self.next = next
+
+    def get_data(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next
+
+class LinkedList(object):
+    def __init__(self, head = None):
+        self.head = head
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def dump(self):
+        temp = self.head
+        while(temp != None):
+            print("Node: ", temp.get_data())
+            temp = temp.get_next()
+
+obj = LinkedList()
+obj.insert(5)
+obj.insert(10)
+obj.insert(15)
+obj.insert(20)
+
+obj.dump()
