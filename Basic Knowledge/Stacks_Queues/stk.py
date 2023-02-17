6f4307e8b3a344af52362b51300b480e49d884fe
devops-sagar <pathaksagar16@gmail.com>
2023-02-17 15:36:34 -0800

    implemented stack methods

--- Basic Knowledge/Stacks_Queues/stk.py
+++ Basic Knowledge/Stacks_Queues/stk.py
@@ -1,16 +1,52 @@
-# create a new empty stack
-stack = []
 
-# push items onto the stack
-stack.append(1)
-stack.append(2)
-stack.append(3)
-stack.append(4)
+''' ************************************************************************
+Start of simple Implementation - just to show how to push and pop
+************************************************************************'''
 
-# print the stack contents
-print(stack)
+# from collections import deque
 
-# pop an item off the stack
-x = stack.pop()
-print(x)
-print(stack)
+# stack = deque()
+# # stack = []
+
+
+# stack.append(1)
+# stack.append(2)
+# stack.append(3)
+# stack.append(4)
+
+# print(stack)
+
+# x = stack.pop()
+# print(x)
+# print(stack)
+
+''' ************************************************************************
+end of simple Implementation 
+below you will see different operations using queue module
+************************************************************************'''
+
+from queue import LifoQueue
+
+stack = LifoQueue(maxsize=2)
+
+print(stack.qsize())
+
+print("Before: ",stack)
+
+stack.put('a')
+stack.put('b')
+stack.put('c')
+# stack.put('d')
+# stack.put('e')
+# stack.put('f')
+
+print("After: ",stack)
+
+print("Stack Full T/F: ", stack.full())
+print("Size of stack: ", stack.qsize())
+
+print(stack.get())
+print(stack.get())
+print(stack.get())
+
+print("Stack Empty T/F: ",stack.empty())
