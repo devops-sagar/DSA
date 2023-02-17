a370b9767923320bfb5e72794857367d0256b46b
devops-sagar <pathaksagar16@gmail.com>
2023-02-17 15:50:55 -0800

    implemented the queue structure

--- Basic Knowledge/Stacks_Queues/que.py
+++ Basic Knowledge/Stacks_Queues/que.py
@@ -1,18 +1,74 @@
-from collections import deque
+''' ************************************************************************
+Start of simple Implementation - just to show how to push and pop in a queue
+************************************************************************'''
+# from collections import deque
 
-# create a new empty deque object that will function as a queue
-queue = deque()
+# # create a new empty deque object that will function as a queue
+# queue = deque()
 
-# add some items to the queue
-queue.append(1)
-queue.append(2)
-queue.append(3)
-queue.append(4)
+# # add some items to the queue
+# queue.append(1)
+# queue.append(2)
+# queue.append(3)
+# queue.append(4)
 
-# print the queue contents
-print(queue)
+# # print the queue contents
+# print(queue)
 
-# pop an item off the front of the queue
-x = queue.popleft()
-print(x)
-print(queue)
+# # pop an item off the front of the queue
+# x = queue.popleft()
+# print(x)
+# print(queue)
+
+''' ************************************************************************
+End of simple Implementation - just to show how to push and pop in a queue
+************************************************************************'''
+
+
+# Although its stack variable .pop(0) makes it act like queue
+stack = []
+  
+stack.append('a')
+stack.append('b')
+stack.append('c')
+  
+print("Initial queue")
+print(stack)
+  
+print("\nElements dequeued from queue")
+print(stack.pop(0))
+print(stack.pop(0))
+print(stack.pop(0))
+  
+print("\nQueue after removing elements")
+print(stack)
+  
+# ------------------------------------------------------------------------------------------------
+
+# Python program to
+# demonstrate implementation of
+# queue using queue module
+
+
+from queue import Queue
+
+q = Queue(maxsize = 3)
+
+print(q.qsize())
+
+q.put('a')
+q.put('b')
+q.put('c')
+
+print("\nFull: ", q.full())
+
+print("\nElements dequeued from the queue")
+print(q.get())
+print(q.get())
+print(q.get())
+
+print("\nEmpty: ", q.empty())
+
+q.put(1)
+print("\nEmpty: ", q.empty())
+print("Full: ", q.full())
