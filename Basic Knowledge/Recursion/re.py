f44b64c81699438b2e692fc5e39b4209c09f5223
devops-sagar <pathaksagar16@gmail.com>
2023-02-19 16:04:07 -0800

    added recursion examples

--- Basic Knowledge/Recursion/re.py
+++ Basic Knowledge/Recursion/re.py
@@ -1,1 +1,37 @@
-sfg
+''' ************************************************************************
+Start of simple Implementation - just to show how recursion works
+************************************************************************'''
+# def countdown(x):
+#     if (x == 0):
+#         print("DOneeeee")
+#         # return
+#     else:
+#         print(x, "...")
+#         countdown(x-1)
+#         print("ok")
+# countdown(5)
+''' ************************************************************************
+end of simple Implementation - just to show how recursion works
+************************************************************************'''
+
+
+
+def power(num, pwr):
+    if pwr == 0:
+        return 1
+    else:
+        # return num**pwr
+        return num * power(num, pwr-1)
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+
+print("{} to the power of {} is {}".format(5, 3, power(5, 3)))
+print("{} to the power of {} is {}".format(1, 5, power(1, 5)))
+print("{}! is {}".format(4, factorial(4)))
+print("{}! is {}".format(0, factorial(0)))
