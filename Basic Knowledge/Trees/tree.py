6ce3856fc8f7f3d8c04e28a0dccc1e36dc12e69b
devops-sagar <pathaksagar16@gmail.com>
2023-02-24 11:45:14 -0800

    added explanation

--- Basic Knowledge/Trees/tree.py
+++ Basic Knowledge/Trees/tree.py
@@ -1,10 +1,10 @@
 # Binary Search Tree
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, left=None, right=None):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
 node = Node(10)
 
@@ -12,3 +12,25 @@ node.left = Node(5)
 node.right = Node(15)
 
 node.left.left = Node(2)
+node.left.right = Node(6)
+
+node.right.left = Node(13)
+node.right.right = Node(1000)
+
+'''
+Line 9 to 18 will generate the following tree:
+
+                                            10(node)
+                                           /  \   
+                               (node.left)5    15(node.right)
+                                         / \   / \
+                        (node.left.left)2   6 13  1000 (node.right.right)
+                            (node.left.right)(node.right.left)
+
+'''
+
+print(node.right.data)
+
+
+my_tree = node.left
+print(my_tree.data)
